32d767f0b1858d302d16ef879cf9cb949a463b7a
Fardan Syed <fardansyed@Fardans-MacBook-Air.local>
2023-10-09 17:20:49 +0530

    added quiz db

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import quizzes from "./db/quizes";
 const app = express();
 const PORT = 3000;
 app.get("/", (req, res) => {
     res.send('hello fardan');
+});
+app.get('/quiz', (req,res) => {
+    res.send(quizzes);
 });
 app.listen(process.env.PORT || PORT, () => {
     console.log('server started');
